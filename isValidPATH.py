b1033e85ace6b0b97fe010376765553ba76305a3
random-toto <myghmail@jetable.fr.nf>
2015-04-15 00:04:25 +0200

    corrigé erreur. commenté.

--- isValidPATH.py
+++ isValidPATH.py
@@ -15,45 +15,64 @@ def isValidPATH(chemin):
     
     '''
     var = str(chemin)   # local, (and str() var)
+    # tests rapide d'exclusion.
     if var == "":
         return -1
     if "..." in var:
         return -1
     if var == "." or var == "..":
         return 1
+    # On commence par retirer le '/' du début s'il existe...
+    # Ça sert à lever l'ambiguïté : '/tmp' de './tmp' facilement. 
     if var[0] == '/':
         os.chdir('/')
         var = var[1:]
     slashes = var.count('/')
+    # CAS 1 : il y a des '/' restants...
     if slashes:
-        var = var.replace('//', '/')
+        var = var.replace('//', '/')    # WARNING : HYPOTHÈSE qui marche sous python3.
+        # SOUS-CAS 1 : il y a un trailing '/' => pas de nom de fichier spécifié.
         if var[-1] == "/":
             try:
                 os.chdir(var)
                 return 1
+                # C'est bien un répertoire.
             except:
                 return -1
+                # Ce n'est pas un répertoire valide. (Au moins sur cette machine).
+        # SOUS-CAS 2 :il n'y a pas de '/' à la fin, la fin peut-être soit un fichier, soit un répertoire. 
         else:
             Liste = var.split('/')
             lastItem = Liste[-1]
             Liste.pop()
+            # On teste sur tous les élément de la liste saus le dernier.
             for i in Liste:
                 try:
                     os.chdir(str(i))
                 except:
                     return -1
+            # On teste le dernier élément de la liste (à part, parce que n'ayant pas de '/' à la fin, ça peut être un fichier ou un dossier).
             if lastItem in os.listdir():
-                return 2
+                try:
+                    os.chdir(str(lastItem))
+                    return 1
+                    # C'est un répertoire.
+                except:
+                    return 2
+                    # C'est un fichier.
             else:
                 return -2
-        
+                # Le dernier élément n'est pas valide, mais le PATH si.
+    # CAS 2 : il n'y en a pas.    
     else:
         try:
             os.chdir(var)
             return 1
+            # Il s'agit d'un répertoire.
         except:
             if var in os.listdir():
                 return 0
+                # Il s'agit d'un fichier.
 
 """
 # Tests unitaires.
